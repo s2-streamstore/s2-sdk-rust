1233cc50a81a99bd6d8d28694f0d02ad05ebbf37
Mehul Arora <mehul_arora@live.com>
2024-09-13 11:58:55 -0400

    ..

--- src/client.rs
+++ src/client.rs
@@ -150,8 +150,8 @@ impl ClientInner {
         match self.config.url.cell.clone() {
             Some(uri) if self.config.url.prefix_host_with_basin => {
                 let host = uri.host().ok_or(ClientError::MissingHost)?;
-                let port = uri.port_u16().map_or(String::new(), |p| p.to_string());
-                let authority: Authority = format!("{basin}.{host}:{port}").parse()?;
+                let port = uri.port_u16().map_or(String::new(), |p| format!(":{}", p));
+                let authority: Authority = format!("{basin}.{host}{port}").parse()?;
                 let mut uri_parts = uri.into_parts();
                 uri_parts.authority = Some(authority);
 
