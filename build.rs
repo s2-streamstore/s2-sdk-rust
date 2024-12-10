56d466f2263b850b4f8dee3596e9922825164266
Mehul Arora <mehul_arora@live.com>
2024-12-10 18:30:07 -0500

    feat: make protoc requirement optional

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .bytes(["."])
-        .compile_protos(&["proto/s2/v1alpha/s2.proto"], &["proto"])?;
-
-    println!("cargo:rustc-env=COMPILED_PROST_FILE=s2.v1alpha.rs");
+    #[cfg(feature = "prost-build")]
+    {
+        tonic_build::configure()
+            .out_dir("src")
+            .bytes(["."])
+            .compile_protos(&["proto/s2/v1alpha/s2.proto"], &["proto"])?;    
+        std::fs::rename("src/s2.v1alpha.rs", "src/api.rs")?;        
+    }
+    println!("cargo:rustc-env=COMPILED_PROST_FILE=src/api.rs");
     Ok(())
 }
