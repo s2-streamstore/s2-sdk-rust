f0a9959be09a00536a0deaf0520325598f4e0d98
Vaibhav Rabber <vaibhav@s2.dev>
2024-12-13 22:35:49 +0530

    create stream
    Signed-off-by: Vaibhav Rabber <vaibhav@s2.dev>

---
+++ examples/create_stream.rs
@@ -0,0 +1,26 @@
+use streamstore::{
+    client::{Client, ClientConfig},
+    types::{BasinName, CreateStreamRequest, StorageClass, StreamConfig},
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let token = std::env::var("S2_AUTH_TOKEN")?;
+    let config = ClientConfig::new(token);
+    let client = Client::new(config);
+
+    let basin: BasinName = "my-basin".parse()?;
+    let basin_client = client.basin_client(basin);
+
+    let stream = "my-stream";
+
+    let stream_config = StreamConfig::new().with_storage_class(StorageClass::Express);
+
+    let create_stream_request = CreateStreamRequest::new(stream).with_config(stream_config);
+
+    let created_stream = basin_client.create_stream(create_stream_request).await?;
+
+    println!("{created_stream:#?}");
+
+    Ok(())
+}
