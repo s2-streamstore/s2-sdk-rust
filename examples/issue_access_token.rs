64e3aa02f3c662d438713025335904281a025f67
Mehul Arora <mehul@s2.dev>
2025-04-18 18:02:21 +0530

    .

--- examples/issue_access_token.rs
+++ examples/issue_access_token.rs
@@ -1,8 +1,8 @@
 use s2::{
     client::{Client, ClientConfig},
     types::{
-        AccessTokenId, AccessTokenInfo, AccessTokenScope, PermittedOperationGroups,
-        ReadWritePermissions,
+        AccessTokenId, AccessTokenInfo, AccessTokenScope, Operation, PermittedOperationGroups,
+        ReadWritePermissions, ResourceSet,
     },
 };
 
@@ -14,10 +14,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let access_token_id: AccessTokenId = "my-access-token".parse()?;
     let access_token_info = AccessTokenInfo::new(access_token_id).with_scope(
-        AccessTokenScope::new().with_op_groups(
-            PermittedOperationGroups::new()
-                .with_account(ReadWritePermissions::new().with_read(true)),
-        ),
+        AccessTokenScope::new()
+            .with_op_groups(
+                PermittedOperationGroups::new()
+                    .with_account(ReadWritePermissions::new().with_read(true)),
+            )
+            .with_op(Operation::CreateStream)
+            .with_streams(ResourceSet::Prefix("my-stream-prefix".to_string()))
+            .with_basins(ResourceSet::Exact("my-perfect-basin".to_string())),
     );
     let token = client.issue_access_token(access_token_info).await?;
 
